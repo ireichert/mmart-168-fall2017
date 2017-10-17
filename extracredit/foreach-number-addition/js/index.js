ec1c85deab39714cda599743852eae4758b699c9
Sarah <vanwars@gmail.com>
2017-10-16 19:02:10 -0700

    added second exercise

---
+++ extracredit/foreach-number-addition/js/index.js
@@ -0,0 +1,5 @@
+/*
+Write some code that adds the following numbers using a forEach loop. Then output the answer inside the span element with the id="answer"
+*/
+
+const numbers = [45, 76, 23, 89, 12, 5, 9, 100, 34]
